2e80d211af10c8133ddcfdd0dbf1553b65989348
sagar-bei031 <078bei031.sagar@pcampus.com>
2024-04-12 02:16:44 +0545

    bnoo8: use gyro, accel & send packet

--- bn008_esp/bn008_esp.ino
+++ bn008_esp/bn008_esp.ino
@@ -24,8 +24,6 @@
 // but not for I2C or UART
 #define BNO08X_RESET 21  // -1
 
-// #define FAST_MODE
-
 // esp32
 #define BN008x_P0 25
 #define BN008x_P1 26
@@ -49,25 +47,28 @@ struct euler_t {
   float yaw;
   float pitch;
   float roll;
-} ypr, offset_ypr, rel_ypr;
+} ypr, offset_ypr;
+
+struct vector3f {
+  float x;
+  float y;
+  float z;
+};
+
+struct robot_state {
+  euler_t orientation;
+  vector3f velocity;
+  vector3f accel;
+} rstate;
 
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
-uint8_t sending_packet[2 + sizeof(rel_ypr)];
+
+uint8_t sending_packet[2 + sizeof(rstate)];
 
 CRC8 crc8(7);
 
-#ifdef FAST_MODE
-// Top frequency is reported to be 1000Hz (but freq is somewhat variable)
-sh2_SensorId_t reportType = SH2_GYRO_INTEGRATED_RV;
-long reportIntervalUs = 2000;
-#else
-// Top frequency is about 250Hz but this report is more accurate
-sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
-long reportIntervalUs = 5000;
-#endif
-
-void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr) {
   float sqr = sq(qr);
   float sqi = sq(qi);
   float sqj = sq(qj);
@@ -76,71 +77,115 @@ void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, boo
   ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
   ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
   ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
-
-  if (degrees) {
-    ypr->yaw *= RAD_TO_DEG;
-    ypr->pitch *= RAD_TO_DEG;
-    ypr->roll *= RAD_TO_DEG;
-  }
 }
 
-void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees = false) {
-  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr) {
+  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr);
 }
 
-void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr, bool degrees = false) {
-  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr) {
+  quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr);
 }
 
-void setReports(sh2_SensorId_t reportType, long report_interval) {
+void setReport(sh2_SensorId_t reportType) {
   Serial.println("Setting desired reports");
-  if (!bno08x.enableReport(reportType, report_interval)) {
+  if (!bno08x.enableReport(reportType)) {
     Serial.println("Could not enable stabilized remote vector");
   }
 }
 
-bool read_data() {
-  bool is_read = false;
-
-  if (bno08x.wasReset()) {
-    Serial.print("sensor was reset ");
-    setReports(reportType, reportIntervalUs);
+// Here is where you define the sensor outputs you want to receive
+void setReports(void) {
+  if (!bno08x.enableReport(SH2_ARVR_STABILIZED_RV)) {
+    Serial.println("Could not enable stabilized remote vector");
   }
-
-  if (bno08x.getSensorEvent(&sensorValue)) {
-    // in this demo only one report type will be received depending on FAST_MODE define (above)
-    switch (sensorValue.sensorId) {
-      case SH2_ARVR_STABILIZED_RV:
-        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
-      case SH2_GYRO_INTEGRATED_RV:
-        // faster (more noise?)
-        quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
-        break;
-    }
-    is_read = true;
+  Serial.println("Setting desired reports");
+  // if (!bno08x.enableReport(SH2_ACCELEROMETER)) {
+  //   Serial.println("Could not enable accelerometer");
+  // }
+  if (!bno08x.enableReport(SH2_GYROSCOPE_CALIBRATED)) {
+    Serial.println("Could not enable gyroscope");
   }
-
-  return is_read;
+  // if (!bno08x.enableReport(SH2_MAGNETIC_FIELD_CALIBRATED)) {
+  //   Serial.println("Could not enable magnetic field calibrated");
+  // }
+  if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION)) {
+    Serial.println("Could not enable linear acceleration");
+  }
+  // if (!bno08x.enableReport(SH2_GRAVITY)) {
+  //   Serial.println("Could not enable gravity vector");
+  // }
+  // if (!bno08x.enableReport(SH2_ROTATION_VECTOR)) {
+  //   Serial.println("Could not enable rotation vector");
+  // }
+  // if (!bno08x.enableReport(SH2_GEOMAGNETIC_ROTATION_VECTOR)) {
+  //   Serial.println("Could not enable geomagnetic rotation vector");
+  // }
+  // if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+  //   Serial.println("Could not enable game rotation vector");
+  // }
 }
 
-void caliberate(uint16_t n = 20) {
+void calibrate(uint16_t n = 20) {
   offset_ypr.yaw = 0.0f;
   offset_ypr.pitch = 0.0f;
   offset_ypr.roll = 0.0f;
 
   int i = 0;
   while (i < n) {
-    if (read_data()) {
-      offset_ypr.yaw += ypr.yaw;
-      offset_ypr.pitch += ypr.pitch;
-      offset_ypr.roll += ypr.roll;
-      i++;
+    if (bno08x.wasReset()) {
+      Serial.print("sensor was reset ");
+      setReport(SH2_ARVR_STABILIZED_RV);
+    }
+
+    if (bno08x.getSensorEvent(&sensorValue)) {
+      if (sensorValue.sensorId == SH2_ARVR_STABILIZED_RV) {
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr);
+        offset_ypr.yaw += ypr.yaw;
+        offset_ypr.pitch += ypr.pitch;
+        offset_ypr.roll += ypr.roll;
+        i++;
+
+        delay(10);
+      }
     }
   }
 
   offset_ypr.yaw /= n;
   offset_ypr.pitch /= n;
   offset_ypr.roll /= n;
+}
+
+void read_data() {
+
+  if (bno08x.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports();
+  }
+
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    // in this demo only one report type will be received depending on FAST_MODE define (above)
+    switch (sensorValue.sensorId) {
+      case SH2_ARVR_STABILIZED_RV:
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr);
+        rstate.orientation.yaw = ypr.yaw - offset_ypr.yaw;
+        rstate.orientation.pitch = ypr.pitch - offset_ypr.pitch;
+        rstate.orientation.roll = ypr.roll - offset_ypr.roll;
+        break;
+
+      case SH2_GYROSCOPE_CALIBRATED:
+        rstate.velocity.x = sensorValue.un.gyroscope.x;
+        rstate.velocity.y = sensorValue.un.gyroscope.y;
+        rstate.velocity.z = sensorValue.un.gyroscope.z;
+        break;
+
+      case SH2_LINEAR_ACCELERATION:
+        rstate.accel.x = sensorValue.un.linearAcceleration.x;
+        rstate.accel.y = sensorValue.un.linearAcceleration.y;
+        rstate.accel.z = sensorValue.un.linearAcceleration.z;
+        break;
+    }
+  }
 }
 
 void setup(void) {
@@ -169,30 +214,26 @@ void setup(void) {
   }
   Serial.println("BNO08x Found!");
 
-  setReports(reportType, reportIntervalUs);
-
-  Serial.print("Caliberating");
+  setReport(SH2_ARVR_STABILIZED_RV);
+  Serial.print("Calibrating");
   delay(100);
-  caliberate(20);
+  calibrate(20);
 
+  setReports();
   Serial.println("Reading events");
 }
 
 void loop() {
-  read_data();
-
   static unsigned long last_tick = 0;
   unsigned long now = micros();
   unsigned long dt = now - last_tick;
 
   if (dt > 30000) {
-    rel_ypr.yaw = ypr.yaw - offset_ypr.yaw;
-    rel_ypr.pitch = ypr.pitch - offset_ypr.pitch;
-    rel_ypr.roll = ypr.roll - offset_ypr.roll;
+  read_data();
 
     sending_packet[0] = START_BYTE;
-    memcpy(sending_packet + 1, (uint8_t*)&rel_ypr, sizeof(rel_ypr));
-    sending_packet[1 + sizeof(rel_ypr)] = crc8.get_hash((uint8_t*)&rel_ypr, sizeof(rel_ypr));
+    memcpy(sending_packet + 1, (uint8_t*)&rstate, sizeof(rstate));
+    sending_packet[1 + sizeof(rstate)] = crc8.get_hash((uint8_t*)&rstate, sizeof(rstate));
     Serial2.write(sending_packet, sizeof(sending_packet));
 
     last_tick = now;
@@ -203,11 +244,27 @@ void loop() {
 
     Serial.print(sensorValue.status);
     Serial.print("\t");  // This is accuracy in the range of 0 to 3
-    Serial.print(ypr.yaw - offset_ypr.yaw);
+
+    Serial.print(rstate.orientation.yaw * RAD_TO_DEG);
     Serial.print("\t");
-    Serial.print(ypr.pitch - offset_ypr.pitch);
+    Serial.print(rstate.orientation.pitch * RAD_TO_DEG);
     Serial.print("\t");
-    Serial.println(ypr.roll - offset_ypr.roll);
+    Serial.print(rstate.orientation.roll * RAD_TO_DEG);
+    Serial.print("\t");
+
+    Serial.print(rstate.velocity.x);
+    Serial.print("\t");
+    Serial.print(rstate.velocity.y);
+    Serial.print("\t");
+    Serial.print(rstate.velocity.z);
+    Serial.print("\t");
+
+    Serial.print(rstate.accel.x);
+    Serial.print("\t");
+    Serial.print(rstate.accel.y);
+    Serial.print("\t");
+    Serial.print(rstate.accel.z);
+    Serial.println();
 #endif
   }
 }
